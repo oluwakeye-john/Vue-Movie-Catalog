ffe5b75508bd39cbb2364c54df04c8018ace24f3
oluwakeye-john <bjohnoluwakeye@gmail.com>
2020-12-14 23:08:45 +0100

    added purge to tailwind for tree-shaking styles in prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: [],
+  // purge: [],
+  purge: ["./public/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
